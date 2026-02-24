5b695ac00368ab93ed7edc2c71db78e4ce151afb
isoul87 <atbrxnk@gmail.com>
2026-02-24 15:24:02 +0700

    update folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/sections/Navbar';
 import Home from './pages/Home'
 import Portfolio from './pages/Portfolio';
 import Services from './pages/Services';
